e324bfb604b0de46975db7a263a331aaf14bc276
jajatda <jajatd4r@gmail.com>
2026-07-07 12:27:44 +0300

    add unstable channel to flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # nixpkgs.url = "nixpkgs/nixos-25.05";
     nixpkgs.url = "nixpkgs/nixos-25.11";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       # url = "github:nix-community/home-manager";
       url = "github:nix-community/home-manager/release-25.11";
