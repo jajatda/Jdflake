2e733dfd54159e0e3ba7f8cc28a707d4c3365734
jajatda <jajatd4r@gmail.com>
2026-07-15 09:44:39 +0300

    Add Php module

---
+++ modules/php.nix
@@ -0,0 +1,36 @@
+# modules/php.nix
+{ config, pkgs, ... }:
+
+let
+  # Merakit PHP kustom beserta ekstensinya
+  myPhp = pkgs.php83.buildEnv {
+    extensions = { enabled, all }: enabled ++ (with all; [
+      bcmath
+      curl
+      dom
+      fileinfo
+      filter
+      gd
+      mbstring
+      openssl
+      pdo
+      pdo_mysql
+      zip
+    ]);
+    extraConfig = ''
+      memory_limit = 2G
+    '';
+  };
+
+  # Menyelaraskan Composer dengan PHP kustom di atas
+  myComposer = pkgs.php83Packages.composer.override { php = myPhp; };
+in
+{
+  # Paket-paket yang akan dipasang ke sistem jika modul ini aktif
+  environment.systemPackages = [
+    myPhp
+    myComposer
+    pkgs.git
+    pkgs.unzip
+  ];
+}
